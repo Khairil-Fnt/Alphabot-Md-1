93d2411d0fd182a5ce20b575374eb7d5e5e5de8f
ᴹᴿᤎ KhairilOfcི ࿇ <90993998+Khairil-Fnt@users.noreply.github.com>
2023-02-24 16:35:16 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ global.packname = '© Bebanbot愛' //sticker wm ubah
 global.author = 'by Vincenzoo' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
-global.typemenu = 'document' //
+global.typemenu = 'Gif' //
 //type menu for downloadmenu, animemenu, etc
 global.typemenu2 = 'templateImage' //
 global.multiplier = 69
